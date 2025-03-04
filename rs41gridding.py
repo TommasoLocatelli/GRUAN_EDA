90d400185fb224cf5ba3a0d20d82b4b68f172b07
TommasoLocatelli <tommaso.locatelli.240397@gmail.com>
2025-03-04 09:54:20 +0100

    no .nc please

---
+++ rs41gridding.py
@@ -0,0 +1,22 @@
+# imports
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# helpers
+from helpers.download_manager.download_manager import DownloadManager
+DM=DownloadManager()
+from helpers.format_manager.format_manager import FormatManager
+FM=FormatManager()
+
+# COSTANTS
+
+# download documentation
+
+# download gdp
+# 2017 zip at https://www.gruan.org/data/file-archive/rs41-gdp1-at-lc
+
+# read data
+
+# Variable Spatial Gridding
+
+# Variable Temporal Gridding
